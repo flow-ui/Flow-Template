94dc98083aae0c69e2586f8d780d4203bb55ed3f
tower1229 <tower1229@qq.com>
2017-03-27 16:00:09 +0800

    update city-select.js

--- _src/lib/seajs/manifest.js
+++ _src/lib/seajs/manifest.js
@@ -19,7 +19,7 @@
         'base'                              : 'v3.3.0',
         'bdshare'                           : 'v3.1.2',
         'box'                               : 'v3.11.1',
-        'city-select'                       : 'v0.1.0',
+        'city-select'                       : 'v1.0.0',
         'countdown'                         : 'v1.1.1',
         'datepicker'                        : 'v1.0.2',
         'drag'                              : 'v0.7.2',
