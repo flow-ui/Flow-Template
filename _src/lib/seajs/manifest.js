e492662501cda359976c26639c8093e73285eeaa
tower1229 <tower1229@qq.com>
2017-03-30 15:27:31 +0800

    update timepicker.js

--- _src/lib/seajs/manifest.js
+++ _src/lib/seajs/manifest.js
@@ -60,7 +60,7 @@
         'slide'                             : 'v4.2.0',
         'tab'                               : 'v3.0.0',
         'table'                             : 'v1.1.2',
-        'timepicker'                        : 'v0.1.0',
+        'timepicker'                        : 'v0.1.1',
         'tip'                               : 'v1.4.0',
         'touch'                             : 'v0.1.1',
         'zoom'                              : 'v2.0.3'
