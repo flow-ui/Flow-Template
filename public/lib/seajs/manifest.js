a38d0214d3351d9fe35c5129283b0c65c954e0f9
tower1229 <tower1229@qq.com>
2017-05-03 16:12:39 +0800

    update table.js

--- public/lib/seajs/manifest.js
+++ public/lib/seajs/manifest.js
@@ -65,7 +65,7 @@
         'spin'                              : 'v0.0.2',
         'switch'                            : 'v0.4.0',
         'tab'                               : 'v4.1.1',
-        'table'                             : 'v1.5.4',
+        'table'                             : 'v1.5.5',
         'timepicker'                        : 'v0.1.2',
         'tip'                               : 'v1.5.0',
         'touch'                             : 'v0.1.1',
