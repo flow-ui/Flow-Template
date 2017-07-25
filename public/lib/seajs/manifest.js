54d843198bf2422edef821d851f342a51ba5c235
tower1229 <tower1229@qq.com>
2017-07-25 16:51:33 +0800

    update paging-load.js

--- public/lib/seajs/manifest.js
+++ public/lib/seajs/manifest.js
@@ -48,7 +48,7 @@
         'offcanvas'                         : 'v2.0.4',
         'on-scroll'                         : 'v2.2.0',
         'page'                              : 'v1.0.6',
-        'paging-load'                       : 'v0.1.0',
+        'paging-load'                       : 'v0.2.0',
         'pjax'                              : 'v0.0.1',
         'progress'                          : 'v0.0.3',
         'qr'                                : 'v0.1.0',
