bebdda1a58a36cc82f4421a317ec1ad818c1190e
tower1229 <tower1229@qq.com>
2017-07-26 14:32:05 +0800

    update scroll-load.js

--- public/lib/seajs/manifest.js
+++ public/lib/seajs/manifest.js
@@ -56,7 +56,7 @@
         'responsive'                        : 'v0.0.1',
         'scroll-bar'                        : 'v2.2.8',
         'scroll-col'                        : 'v4.2.4',
-        'scroll-load'                       : 'v1.0.1',
+        'scroll-load'                       : 'v1.0.2',
         'scroll-row'                        : 'v3.0.6',
         'select'                            : 'v4.3.5',
         'sendcode'                          : 'v0.2.1',
