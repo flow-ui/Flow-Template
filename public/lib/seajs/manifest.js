594969ed9d2d35d3832b6cdf8d766d159a00152a
tower1229 <tower1229@qq.com>
2017-08-23 10:08:49 +0800

    update drag.js

--- public/lib/seajs/manifest.js
+++ public/lib/seajs/manifest.js
@@ -25,7 +25,7 @@
         'collapse'                          : 'v0.0.2',
         'countdown'                         : 'v1.1.1',
         'datepicker'                        : 'v2.0.2',
-        'drag'                              : 'v0.10.1',
+        'drag'                              : 'v0.10.4',
         'drag-panel'                        : 'v0.0.2',
         'dropdown'                          : 'v0.2.3',
         'easing'                            : 'v0.0.1',
